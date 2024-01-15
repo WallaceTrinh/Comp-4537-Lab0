e0d1113713945e58bee10fd29250981d02ac9cac
Wallace Trinh <97257455+WallaceTrinh@users.noreply.github.com>
2024-01-14 23:48:21 -0800

    updating files

--- js/script.js
+++ js/script.js
@@ -1,21 +1,5 @@
-// Firebase Functions
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
-// Web app's Firebase Configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyA1MNSGhQaaUN7IcIINWOmcaTkcjMpgn6E",
-  authDomain: "wallacetrinh-comp4537-la-67efb.firebaseapp.com",
-  projectId: "wallacetrinh-comp4537-la-67efb",
-  storageBucket: "wallacetrinh-comp4537-la-67efb.appspot.com",
-  messagingSenderId: "91192378604",
-  appId: "1:91192378604:web:20a0ba15facf04459f91c9",
-  measurementId: "G-LFHKFMRF8K"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Import the Firebase configuration
+import { app, analytics } from "./firebase-config";
 
 class Button {
     constructor(color, width, height, order) {
